fd7561eb737be0e51afefe8f58378e3ae29746aa
benassijosef1 <jbenassi@zaizi.com>
2020-11-28 18:31:55 +0000

    cypress in

---
+++ cypress/integration/intergration.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+describe("Cypress", () => {
+  it("is working", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("opens the app", () => {
+    cy.visit("http://localhost:3000");
+  });
+});
